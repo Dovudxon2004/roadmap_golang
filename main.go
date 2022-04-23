aae7572511dac307d4fdfc3a6634b4090703af2b
Dovudxon <dovudxonabduboqiyev@gmail.com>
2022-04-23 06:07:35 +0500

    Somethin

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Dovudxon2004/roadmap_golang/roadmap"
 )
 
 func main() {
-	fmt.Println(roadmap.HelloString())
+	roadmap.HelloString()
 }
--- roadmap/variables.go
+++ roadmap/variables.go
@@ -1,5 +1,18 @@
 package roadmap
 
-func HelloString() string {
-	return "Hello World"
+import (
+	"fmt"
+	"math"
+)
+
+func HelloString() {
+
+	pow := func(x, n, lim float64) float64 {
+		if v := math.Pow(x, n); v < lim {
+			return v
+		}
+		return lim
+	}
+	fmt.Println(pow(2, 5, 31))
+	fmt.Println(pow(2, 5, 33))
 }
